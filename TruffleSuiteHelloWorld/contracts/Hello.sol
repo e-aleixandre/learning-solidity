9d6f0f1562ba09415d572288112db36a45ca09f4
EAleixandre <ealeixandremp@gmail.com>
2021-12-26 23:34:06 +0100

    Advanced on the course

--- TruffleSuiteHelloWorld/contracts/Hello.sol
+++ TruffleSuiteHelloWorld/contracts/Hello.sol
@@ -1,7 +1,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Hello {
-    function hello() external pure returns (string memory) {
-        return "Hello world";
+    string _message;
+
+    function setMessage(string memory message) public payable{
+        _message = message;
+    }
+
+    function hello() external view returns (string memory) {
+        return _message;
     }
 }
